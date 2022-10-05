cb4038a204031ae0743db0cc20c2da8795a053be
Subhi YD <subhy809fr@gmail.com>
2022-10-05 13:22:25 +0200

    11ème commit: rajout HttpClientModule, import dans app.module, Créé angular-in-memory-web-api, InMemoryDbService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // fournir les éléments essentiels pour le fonctionnement de l'app, par exemple, le directive egi
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { FormsModule } from '@angular/forms';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({ // déclarer le module auprès de Angular
   declarations: [ // une liste des tous les composants 
@@ -15,9 +18,10 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule, // importer d'autres modules 
     FormsModule, // injecter le FormsModule au niveau de racine
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false}),
     PokemonModule,
     AppRoutingModule
-    
   ],
   providers: [], // injection dépendance d'angular
   bootstrap: [AppComponent] // une option, composant racine, lancer dès le démarrage de l'app
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api'; // Ajouter une interface (appelée: in-memory-db-service ), qui nous demande d’implémenter une méthode pour simuler notre base de données 
+import { POKEMONS } from './pokemon/mock-pokemon-list';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+
+  createDb() {
+      return { POKEMONS };
+  }
+}
