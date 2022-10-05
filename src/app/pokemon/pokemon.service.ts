7e8038a4ecb88ec185047678085f30b51588b15f
Subhi YD <subhy809fr@gmail.com>
2022-10-05 13:48:31 +0200

    12ème commit: créé branche  dev_sans_serveur_distant_05102022, modification pokemon.service pour requêtes http

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,17 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { Pokemon } from './pokemon';
 
 @Injectable()
 
 export class PokemonService { // créer un service, 3 méthodes 1. getPokemonList, 2.getPokemonById(id), 3.getPokemonTypeList
 
-  getPokemonList(): Pokemon[] {
-    return POKEMONS;
+  constructor(private http: HttpClient) {}
+
+  getPokemonList(): Observable<Pokemon[]> {
+    //return POKEMONS;
+
+    // retourner un flux
+    return this.http.get<Pokemon[]>('api/pokemons').pipe( // plus d'infos
+      tap((pokemonList) => console.table(pokemonList)),
+      catchError((error) => {
+        console.log(error);
+        return of([]); // retourne un tableau vide pour ne pas cracher l'app
+      })
+    );
   }
 
-  getPokemonById(pokemonId: number): Pokemon | undefined {
-    return POKEMONS.find(pokemon => pokemon.id == pokemonId);
+  getPokemonById(pokemonId: number): Observable<Pokemon|undefined> {
+    //return POKEMONS.find(pokemon => pokemon.id == pokemonId);
+    return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
+      tap((pokemon) => console.table(pokemon)),
+      catchError((error) => {
+        console.log(error);
+        return of(undefined); // retourne un undefind en cas d'aucun pokémon pour ne pas cracher l'app
+      })
+    );
   } 
   getPokemonTypeList(): string[] {
     return [
